20b5d3a77b8004b12ebc465169f760107d938d43
phyxdown <1768649634@qq.com>
2015-08-26 17:04:05 +0800

    combine list & stack

---
+++ ds.c
@@ -0,0 +1,160 @@
+/* __LIST__ */
+#ifndef __LIST__
+#define __LIST__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ds.h"
+
+list*
+listCreate() {
+	list* list;
+	if ((list = malloc(sizeof(*list))) == NULL) 
+		return NULL;
+	list->head = list->tail = NULL;
+	list->len = 0;
+	list->dup = NULL;
+	list->free = NULL;
+	list->match = NULL;
+	return list;
+}
+
+void
+listRelease(list* list) {
+	size_t len;
+	listNode* current;
+	listNode* next;
+
+	current = list->head;
+	len = list->len;
+	while(len--) {
+		next = current->next;
+		if (list->free) list->free(current->value);
+		free(current);
+		current = next;
+	}
+	free(list);
+}
+
+list*
+listAddNodeHead(list* list, void* value) {
+	listNode* node;
+	if((node = malloc(sizeof(*node))) == NULL)
+		return NULL;
+	node->value = value;
+	if (list->len == 0) {
+		list->head = list->tail = node;
+		node->prev = node->next = NULL;
+	} else {
+		node->prev = NULL;
+		node->next = list->head;
+		node->next->prev = node;
+		list->head = node;
+	}
+	list->len ++;
+	return list;
+}
+
+list*
+listAddNodeTail(list* list, void* value) {
+	listNode* node;
+	if((node = malloc(sizeof(*node))) == NULL)
+		return NULL;
+	node->value = value;
+	if (list->len = 0 ) {
+		list->head = list->tail = node;
+		node->prev = node->next = NULL;
+	} else {
+		node->prev = list->tail;
+		node->next = NULL;
+		node->prev->next = node;
+		list->tail = node;
+	}
+	list->len ++;
+	return list;
+}
+
+listIter*
+listGetIterator(list* list, int direction) {
+	listIter* iter;
+	if ((iter = malloc(sizeof(*iter))) == NULL) return NULL;
+	if (direction == LIST_START_HEAD) iter->next = list->head;
+	else iter->next = list->tail;
+	iter->direction = direction;
+	return iter;
+}
+
+
+listNode*
+listNext(listIter* iter) {
+	listNode* current = iter->next;
+
+	if (current != NULL) {
+		if (iter->direction == LIST_START_HEAD)
+			iter->next = current->next;
+		else 
+			iter->next = current->prev;
+	}
+	return current;
+}
+
+void
+listReleaseIterator(listIter* iter) {
+	free(iter);
+}
+
+#endif /* __LIST__ */
+
+
+/* __STACK__ */
+#ifndef __STACK__
+#define __STACK__
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ds.h"
+
+stack*
+stackCreate() {
+	stack* stack;
+	if ((stack = malloc(sizeof(*stack))) == NULL) 
+		return NULL;
+	stack->top = NULL;
+	stack->free = NULL;
+	return stack;
+}
+
+void
+stackRelease(stack* stack) {}
+
+void*
+stackPop(stack* stack) {
+	void* value;
+	stackNode* current;
+	if ((current = stack->top) == NULL)
+		return NULL;
+	stack->top = current->next;
+	current->next = NULL;
+	value = current->value;
+	free(current);
+	return value;
+}
+
+void*
+stackPeek(stack* stack) {
+	stackNode* current;
+	if ((current = stack->top) == NULL) return NULL;
+	return current->value;
+}
+
+void*
+stackPush(stack* stack, void* value) {
+	stackNode* node;
+	if ((node = malloc(sizeof(*node))) == NULL) return NULL;
+	node->value = value;
+	node->next = stack->top;
+	stack->top = node;
+	return value;
+}
+#endif /* __STACK__ */
---
+++ ds.h
@@ -0,0 +1,63 @@
+#ifndef __LIST_H__
+#define __LIST_H__
+
+#include <stdio.h>
+
+/* general double linked list */
+
+typedef struct listNode {
+	struct listNode* prev;
+	struct listNode* next;
+	void* value;
+} listNode;
+
+typedef struct listIter {
+	listNode* next;
+	int direction;
+} listIter;
+
+typedef struct list {
+	listNode *head;
+	listNode *tail;
+	size_t len;
+	void* (*dup) (void *ptr);
+	void (*free) (void *ptr);
+	int (*match) (void *ptr, void *key);
+} list;
+
+list* listCreate();
+void  listRelease(list* list);
+list* listAddNodeHead(list* list, void* value);
+list* listAddNodeTail(list* list, void* value);
+
+listIter* listGetIterator(list* list, int direction);
+listNode* listNext(listIter* iter);
+void listReleaseIterator(listIter* iter);
+
+#define LIST_START_HEAD 0
+#define LIST_START_TAIL 1
+
+#endif /* __LIST_H__ */
+
+
+#ifndef __STACK_H__
+#define __STACK_H__
+
+typedef struct stackNode {
+	struct stackNode* next;
+	void* value;
+} stackNode;
+
+typedef struct stack {
+	stackNode *top;
+	void (*free) (void* ptr);
+} stack;
+
+stack* stackCreate();
+void stackRelease(stack* stack);
+
+void* stackPop(stack* stack);
+void* stackPeek(stack* stack);
+void* stackPush(stack* stack, void* value);
+
+#endif /* __STACK_H */
