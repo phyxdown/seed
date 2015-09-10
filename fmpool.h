5ddaddf975a00a7be5f98fecdd9704fb506b7b14
phyxdown <1768649634@qq.com>
2015-09-10 19:39:31 +0800

    fmpool example

--- fmpool.h
+++ fmpool.h
@@ -1,22 +1,41 @@
 #ifndef __SEED_FMPOOL_H
 #define __SEED_FMPOOL_H
 
+/**
+ * EXAMPLE
+ *
+ *	typedef struct {
+ *		size_t age;
+ *		size_t height;
+ *	} wife;
+ *	
+ *	int main() {
+ *		fmpool* p = fmpoolCreate(64, sizeof(wife));
+ *		wife* w = (wife*)fmpoolAlloc(p);
+ *		w->age = 23;
+ *		w->height = 169;
+ *		printf("My wife is %d year's old.", w->age);
+ *		fmpoolFree(w);
+ *	}
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct {
-	int spare;
-	char memory[];
+	int    busy;
+	char   memory[];
 } fobj;
 
 typedef struct {
 	size_t cap;
 	size_t size;
-	char memory[];
+	size_t last;
+	char   memory[];
 } fmpool;
 
 fmpool* fmpoolCreate(size_t cap, size_t size);
-void*   fmpoolGet(fmpool* pool);
+void*   fmpoolAlloc(fmpool* pool);
 void    fmpoolFree(void* obj);
 
 #endif
