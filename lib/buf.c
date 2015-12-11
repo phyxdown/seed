c515b23607588178e3fe8c06070893f77507795d
phyxdown <1768649634@qq.com>
2015-12-11 14:06:14 +0800

    fix a bug about realloc

--- lib/buf.c
+++ lib/buf.c
@@ -30,7 +30,10 @@ buf* bufCat(buf* buffer, const char *add) {
 		else newlen += BUF_MAX_PREALLOC;
 		buf* nbuffer = realloc(buffer, sizeof(struct buf)+ newlen + 1);
 		if (nbuffer == NULL) return buffer;
-		else buffer = nbuffer;
+		else { 
+			if (buffer != nbuffer) free(buffer);
+			buffer = nbuffer;
+		};
 		buffer->free = newlen - buffer->len;
 	}
 	strcpy(&buffer->data[buffer->len], add);
