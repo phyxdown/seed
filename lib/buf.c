a942f6858c7c899f7dd552dc6abedbb165d5c9b9
phyxdown <1768649634@qq.com>
2015-12-14 12:01:22 +0800

    fix a bug of buf

--- lib/buf.c
+++ lib/buf.c
@@ -16,12 +16,13 @@ buf* bufNew(const char *init) {
 	return buffer;
 }
 
-void bufRelease(Buf buf) {
-	if (buf == NULL) return;
-	free(buf);
+void bufRelease(buf *buffer) {
+	if (buffer == NULL) return;
+	free(buffer);
 }
 
-buf* bufCat(buf* buffer, const char *add) {
+buf* bufCat(buf *bufin, const char *add) {
+	buf *buffer = bufin;
 	size_t addlen = strlen(add);
 	if (addlen == 0) return buffer;
 	if (addlen > buffer->free) {
@@ -29,7 +30,10 @@ buf* bufCat(buf* buffer, const char *add) {
 		if (newlen < BUF_MAX_PREALLOC) newlen *= 2;
 		else newlen += BUF_MAX_PREALLOC;
 		buf* nbuffer = realloc(buffer, sizeof(struct buf)+ newlen + 1);
-		if (nbuffer == NULL) return buffer;
+		if (nbuffer == NULL) { 
+			free(buffer);
+			return NULL;
+		}
 		else buffer = nbuffer;
 		buffer->free = newlen - buffer->len;
 	}
@@ -40,7 +44,8 @@ buf* bufCat(buf* buffer, const char *add) {
 	return buffer;
 }
 
-buf* bufVcatf(buf* buffer, const char *fmt, va_list ap) {
+buf* bufVcatf(buf *bufin, const char *fmt, va_list ap) {
+	buf *buffer = bufin;
 	va_list cpy;
 	char staticbuf[1024], *buf = staticbuf;
 	size_t buflen = strlen(fmt)*2;
@@ -69,7 +74,8 @@ buf* bufVcatf(buf* buffer, const char *fmt, va_list ap) {
 	return buffer;
 }
 
-buf* bufCatf(buf* buffer, const char *fmt, ...) {
+buf* bufCatf(buf* bufin, const char *fmt, ...) {
+	buf *buffer = bufin;
 	va_list ap;
 	va_start(ap, fmt);
 		buffer = bufVcatf(buffer, fmt, ap);
