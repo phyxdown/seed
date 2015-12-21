8e439a23a0daf83ca9cde41374bde140f41a1e23
phyxdown <1768649634@qq.com>
2015-12-21 12:17:10 +0800

    change api of seed_net_hostip[x]

--- lib/net.c
+++ lib/net.c
@@ -7,7 +7,7 @@
 #include <arpa/inet.h>
 #include <sys/ioctl.h>
 
-char* seed_net_hostip() {
+int seed_net_hostip(char** pp) {
 	int sockfd;
 	struct ifconf ifconf;
 	char buf[1024];
@@ -16,7 +16,7 @@ char* seed_net_hostip() {
 	ifconf.ifc_len = 1024;
 	ifconf.ifc_buf = buf;
 	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-		return NULL;
+		return -1;
 	}
 	ioctl(sockfd, SIOCGIFCONF, &ifconf);
 	close(sockfd);
@@ -29,25 +29,32 @@ char* seed_net_hostip() {
 			ifreq++;
 			continue;
 		}
-		return strdup(ip);
+		*pp = strdup(ip);
+		return 0;
 	}
-	return NULL;
+	return -1;
 }
 
-char* seed_net_hostipx() {
+int seed_net_hostipx(char** pp) {
 	char* ipd;
-	if ((ipd = seed_net_hostip()) == NULL) return NULL;
-	char ipx[20]; 
-	int t[4];
-	sscanf(ipd, "%d.%d.%d.%d", &t[0], &t[1], &t[2], &t[3]);
-	int i = 0;
-	char *ipxp = ipx;
-	for (i = 0; i < 4; i++) {
-		if (t[i] < 16) {
-			sprintf(ipxp+i*2, "0%x", t[i]);
-		} else {
-			sprintf(ipxp+i*2, "%x", t[i]);
+	int r = seed_net_hostip(&ipd);
+	if (r == 0) {
+		char ipx[20]; 
+		int t[4];
+		sscanf(ipd, "%d.%d.%d.%d", &t[0], &t[1], &t[2], &t[3]);
+		int i = 0;
+		char *ipxp = ipx;
+		for (i = 0; i < 4; i++) {
+			if (t[i] < 16) {
+				sprintf(ipxp+i*2, "0%x", t[i]);
+			} else {
+				sprintf(ipxp+i*2, "%x", t[i]);
+			}
 		}
+		*pp = strdup(ipx);
+		free(ipd);
+		return 0;
+	} else {
+		return -1;
 	}
-	return strdup(ipx);
 }
--- lib/net.h
+++ lib/net.h
@@ -1,7 +1,7 @@
 #ifndef __SEED_NET_H
 #define __SEED_NET_H
 
-char* seed_net_hostip();
-char* seed_net_hostipx();
+int seed_net_hostip(char** pp);
+int seed_net_hostipx(char** pp);
 
 #endif
